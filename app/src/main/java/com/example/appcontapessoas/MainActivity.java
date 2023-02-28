7756a508c025a620cefbbf2a61360d70bd92d66c
leonardolginfo <leonardo@lginfo.com.br>
2023-02-28 07:48:50 -0300

    declaradas variaveis e vinculos feitos

--- app/src/main/java/com/example/appcontapessoas/MainActivity.java
+++ app/src/main/java/com/example/appcontapessoas/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.appcontapessoas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtNumberPeaple;
+        Button btnCountMen;
+        Button btnCountWomen;
+
+        txtNumberPeaple = findViewById(R.id.textNumberpeaple);
+        btnCountMen = findViewById(R.id.btnMasc);
+        btnCountWomen = findViewById(R.id.btnFem);
+
     }
 }
