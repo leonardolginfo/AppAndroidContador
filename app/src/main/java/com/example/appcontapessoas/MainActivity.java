c78ab827144bbf1caae298ae20558da13c99668c
leonardolginfo <leonardo@lginfo.com.br>
2023-02-28 22:30:45 -0300

    refatoracao btns texts layout clicks contagem func ok

--- app/src/main/java/com/example/appcontapessoas/MainActivity.java
+++ app/src/main/java/com/example/appcontapessoas/MainActivity.java
@@ -3,23 +3,46 @@ package com.example.appcontapessoas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int numberMen = 0;
+    int numberWomen = 0;
+    int numberPeaple = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtNumberPeaple;
-        Button btnCountMen;
-        Button btnCountWomen;
+        TextView txtNumberPeaple = (TextView) findViewById(R.id.textNumberpeaple);
+        Button btnCountMen = (Button) findViewById(R.id.btnMasc);
+        Button btnCountWomen = (Button) findViewById(R.id.btnFem);
 
-        txtNumberPeaple = findViewById(R.id.textNumberpeaple);
-        btnCountMen = findViewById(R.id.btnMasc);
-        btnCountWomen = findViewById(R.id.btnFem);
+        btnCountMen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberMen++;
+                numberPeaple++;
+                String txtTempNumberPeaple = Integer.toString(numberPeaple);
+                txtNumberPeaple.setText(txtTempNumberPeaple);
+                btnCountMen.setText(Integer.toString(numberMen));
+            }
+        });
 
+        btnCountWomen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberWomen++;
+                numberPeaple++;
+                String txtTempNumberPeaple = Integer.toString(numberPeaple);
+                txtNumberPeaple.setText(txtTempNumberPeaple);
+                btnCountWomen.setText(Integer.toString(numberWomen));
+            }
+        });
     }
 }
