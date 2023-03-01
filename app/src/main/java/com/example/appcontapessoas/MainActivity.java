a88d644d8c80e358d0d1c515b5dd4e4de127f9b4
leonardolginfo <leonardo@lginfo.com.br>
2023-03-01 05:43:20 -0300

    refatoracao usando lambda e valueof

--- app/src/main/java/com/example/appcontapessoas/MainActivity.java
+++ app/src/main/java/com/example/appcontapessoas/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.appcontapessoas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,38 +23,29 @@ public class MainActivity extends AppCompatActivity {
         Button btnCountWomen = (Button) findViewById(R.id.btnFem);
         Button btnResetCount = (Button) findViewById(R.id.btnReset);
 
-        btnCountMen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberMen++;
-                numberPeaple++;
-                String txtTempNumberPeaple = Integer.toString(numberPeaple);
-                txtNumberPeaple.setText(txtTempNumberPeaple);
-                btnCountMen.setText(Integer.toString(numberMen));
-            }
+        btnCountMen.setOnClickListener(v -> {
+            numberMen++;
+            numberPeaple++;
+            String txtTempNumberPeaple = Integer.toString(numberPeaple);
+            txtNumberPeaple.setText(txtTempNumberPeaple);
+            btnCountMen.setText(String.valueOf(numberMen));
         });
 
-        btnCountWomen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberWomen++;
-                numberPeaple++;
-                String txtTempNumberPeaple = Integer.toString(numberPeaple);
-                txtNumberPeaple.setText(txtTempNumberPeaple);
-                btnCountWomen.setText(Integer.toString(numberWomen));
-            }
+        btnCountWomen.setOnClickListener(v -> {
+            numberWomen++;
+            numberPeaple++;
+            String txtTempNumberPeaple = Integer.toString(numberPeaple);
+            txtNumberPeaple.setText(txtTempNumberPeaple);
+            btnCountWomen.setText(String.valueOf(numberWomen));
         });
 
-        btnResetCount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                numberMen = 0;
-                numberWomen = 0;
-                numberPeaple = 0;
-             txtNumberPeaple.setText(Integer.toString(numberPeaple));
-             btnCountMen.setText(Integer.toString(numberMen));
-             btnCountWomen.setText(Integer.toString(numberWomen));
-            }
+        btnResetCount.setOnClickListener(v -> {
+            numberMen = 0;
+            numberWomen = 0;
+            numberPeaple = 0;
+         txtNumberPeaple.setText(String.valueOf(numberPeaple));
+         btnCountMen.setText(String.valueOf(numberMen));
+         btnCountWomen.setText(String.valueOf(numberWomen));
         });
 
     }
